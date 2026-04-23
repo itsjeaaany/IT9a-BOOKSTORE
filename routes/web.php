36ad4d8083493477d7dd85be2fa4547283389c7f
Jeany Anne Butalid <j.butalid.553781@umindanao.edu.ph>
2026-04-23 16:10:17 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
+Route::get('/', function () {
+    return redirect('/books');
+});
 Route::get('books', [BookController::class, 'index']);
 Route::get('books/create', [BookController::class, 'create']);
 Route::post('books', [BookController::class, 'store']);
